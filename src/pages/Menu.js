6a38d552d197373600d34a31bdf0945f4aafd94c
Sara Rotenski <162700489+sararotenski@users.noreply.github.com>
2025-07-09 09:15:43 -0300

    Update Menu.js

--- src/pages/Menu.js
+++ src/pages/Menu.js
@@ -5,10 +5,8 @@ import Cardapio from '../images/menu-images/cardapio-yuki.jpg';
 function Menu() {
   return (
     <div>
-    <div className='cardapio-container'>
       <p class='menu-text'>NOSSO MENU</p>
       <img className="cardapio" src={ Cardapio } alt="Cardápio"/>
-    </div>
       <footer>
       <Footer />
       </footer>
